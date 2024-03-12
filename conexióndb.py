09fe936befa86e6b99881357ead327c37632c826
MadaGG01 <madagg06@gmail.com>
2024-03-12 10:04:48 -0600

    update

--- conexióndb.py
+++ conexióndb.py
@@ -1,7 +1,10 @@
+from flask import Flask
 import mysql.connector
 import bcrypt
 import random
 import string
+
+app = Flask(__name__)
 
 def create_connection():
     return mysql.connector.connect(
@@ -29,9 +32,7 @@ def generate_random_password(length=12):
     return ''.join(random.choice(characters) for _ in range(length))
 
 def hash_password(password):
-    # Generar un salt aleatorio
     salt = bcrypt.gensalt()
-    # Hashear la contraseña con el salt
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
 
@@ -45,6 +46,38 @@ def insert_usuario(conn, nombre, apellido, nip, email, id_docente, imagen):
     conn.commit()
     return password
 
+def insert_administrador(conn, id_administrador, nombre, apellidos, correo, imagen):
+    cursor = conn.cursor()
+    password = generate_random_password()
+    hashed_password = hash_password(password)
+    sql = '''INSERT INTO Datos_Prof (id_docente, nombre, apellido, email, imagen, contraseña_hash) VALUES (%s, %s, %s, %s, %s, %s)'''
+    values = (id_administrador, nombre, apellidos, correo, imagen, hashed_password)
+    cursor.execute(sql, values)
+    conn.commit()
+    return password
+
+def insert_administrador(conn, id_administrador, nombre, apellidos, correo, imagen):
+    cursor = conn.cursor()
+    password = generate_random_password()
+    hashed_password = hash_password(password)
+    sql = '''INSERT INTO Administradores (id_administrador, nombre, apellidos, correo, imagen, contraseña_hash) VALUES (%s, %s, %s, %s, %s, %s)'''
+    values = (id_administrador, nombre, apellidos, correo, imagen, hashed_password)
+    cursor.execute(sql, values)
+    conn.commit()
+    return password
+
+def insert_materia(conn, subject_name, subject_id):
+    cursor = conn.cursor()
+    sql = '''INSERT INTO Materias (nombre_materia, id_materia) VALUES (%s, %s)'''
+    values = (subject_name, subject_id)
+    cursor.execute(sql, values)
+    conn.commit()
+    
+
 def close_connection(conn):
     conn.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from conexióndb import create_connection, create_table, insert_usuario, close_connection
+from conexióndb import create_connection, create_table, insert_usuario, close_connection, insert_administrador, insert_materia
 
 app = Flask(__name__)
 
@@ -41,7 +41,57 @@ def submit():
 
 @app.route('/administrador')
 def administrador():
+    
     return render_template('administrador.html')
+
+# Ruta para procesar los datos del formulario de administrador
+@app.route('/admin_form', methods=['POST'])
+def submit_admin_form():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        id_administrador = request.form['id_administrador']
+        nombre = request.form['nombre']
+        apellidos = request.form['apellidos']
+        correo = request.form['correo']
+        imagen = request.files['imagen'].read()  # Lee el contenido del archivo de imagen
+        
+        # Conectar a la base de datos
+        conn = create_connection()
+        create_table(conn)  # Asegúrate de que la tabla exista
+
+        # Insertar datos en la base de datos
+        insert_administrador(conn, id_administrador, nombre, apellidos, correo, imagen)
+
+        # Cerrar la conexión
+        close_connection(conn)
+
+        return 'Datos del administrador enviados a la base de datos y correo electrónico enviado con éxito'
+
+@app.route('/Materia')
+def Materia():
+    
+    return render_template('Materia.html')
+
+# Ruta para procesar los datos del formulario de materia
+@app.route('/Materia', methods=['POST'])
+def submit_materia():
+    if request.method == 'POST':
+        # Obtener datos del formulario
+        subject_name = request.form['subject_name']
+        subject_id = request.form['subject_id']
+        
+        # Conectar a la base de datos
+        conn = create_connection()
+        create_table(conn)  # Asegúrate de que la tabla exista
+
+        # Insertar datos en la base de datos
+        insert_materia(conn, subject_name, subject_id)
+
+        # Cerrar la conexión
+        close_connection(conn)
+
+        return 'Materia guardada exitosamente'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
