980a0ec5e102dd79f85f70ee0bf4bf103fe35778
Cherry <fernandotb281005@gmail.com>
2024-04-01 16:14:18 -0600

    Inicio de seción y recuperación de password

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 
-from flask import Flask, render_template, request
-from conexióndb import create_connection, create_table, insert_usuario, close_connection, insert_estudiante, insert_administrador
+from flask import Flask, render_template, request, redirect, url_for
+from conexióndb import create_connection, create_table, insert_usuario, close_connection, insert_estudiante, insert_administrador, send_email, authenticate_user, authenticate_userAdmin
 from facial_recognition import extraer_encodings
+
 
 app = Flask(__name__)
 
@@ -64,6 +65,53 @@ def submit():
     close_connection(conn)
 
     return 'Datos enviados a la base de datos y correo electrónico enviado con éxito'
+  
+
+@app.route('/loginn', methods=['POST'])
+def loginn():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['contraseña']
+
+        user = authenticate_user(email, password)
+
+        if user:
+            # Inicio de sesión exitoso, redireccionar a una página de bienvenida
+            return redirect(url_for('about'))
+        else:
+            # Credenciales incorrectas, redireccionar de nuevo al formulario de inicio de sesión
+            return render_template('login.html', error="Credenciales incorrectas")
+  
+
+@app.route('/getPassword')
+def getPassword():
+  return render_template('getPassword.html')
+  
+
+@app.route('/recuperacion', methods=['GET', 'POST'])
+def recuperacion():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        conn = create_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT contraseña FROM Datos_Prof WHERE email = %s", (email,))
+        contraseña_encontrada = cursor.fetchone()
+
+        if contraseña_encontrada:
+            # Enviar la contraseña tal como está en la base de datos por correo electrónico
+            message = f"Tu contraseña es: {contraseña_encontrada[0]}"
+            if send_email(email, message):
+                mensaje = "Revisa tu correo electronico" 
+                return render_template('login.html', mensaje=mensaje)
+            else:
+                return "Error al enviar correo electrónico. Por favor, inténtelo de nuevo más tarde."
+        else:
+            return "No se encontró ninguna cuenta asociada a ese correo electrónico."
+
+    return render_template('get_password.html')
+
+  
 
 @app.route('/administrador')
 def administrador():
@@ -91,6 +139,50 @@ def submit_admin_form():
         close_connection(conn)
 
         return 'Datos del administrador enviados a la base de datos y correo electrónico enviado con éxito'
+      
+
+@app.route('/getPasswordAdmin')
+def getPasswordAdmin():
+  return render_template('getPasswordAdmin.html')
+
+
+@app.route('/loginnAdmin', methods=['POST'])
+def loginnAdmin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['contraseña']
+
+        user = authenticate_userAdmin(email, password)
+
+        if user:
+            # Inicio de sesión exitoso, redireccionar a una página de bienvenida
+            return redirect(url_for('about'))
+        else:
+            # Credenciales incorrectas, redireccionar de nuevo al formulario de inicio de sesión
+            return render_template('loginAdmin.html', error="Credenciales incorrectas")
+
+@app.route('/recuperacionAdmin', methods=['GET', 'POST'])
+def recuperacionAdmin():
+    if request.method == 'POST':
+        email = request.form['email']
+
+        conn = create_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT contraseña FROM administradores WHERE correo = %s", (email,))
+        contraseña_encontrada = cursor.fetchone()
+
+        if contraseña_encontrada:
+            # Enviar la contraseña tal como está en la base de datos por correo electrónico
+            message = f"Tu contraseña es: {contraseña_encontrada[0]}"
+            if send_email(email, message):
+                mensaje = "Revisa tu correo electronico" 
+                return render_template('login.html', mensaje=mensaje)
+            else:
+                return "Error al enviar correo electrónico. Por favor, inténtelo de nuevo más tarde."
+        else:
+            return "No se encontró ninguna cuenta asociada a ese correo electrónico."
+
+    return render_template('get_password.html')
 
 
 @app.route('/estudiante')
