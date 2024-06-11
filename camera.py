153e9c7db98b547bda91cc2b1fa88752f69c3853
anngell09 <angel.alvanezg09@gmail.com>
2024-06-11 16:45:16 -0600

    videoFrameLuzMovimientoCorregido

--- camera.py
+++ camera.py
@@ -38,6 +38,7 @@ def start_camera():
     if not camera_running:
         last_result = None
         cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)  # Limitar el tamaño del buffer
         camera_running = True
         processing = False
         student_info = None
@@ -87,78 +88,82 @@ def generate():
     last_compare_time = 0
 
     while camera_running:
-        ret, frame = cap.read()
-        if not ret or frame is None:
-            print("Error al capturar el frame")
-            break
+        try:
+            ret, frame = cap.read()
+            if not ret or frame is None:
+                print("Error al capturar el frame")
+                continue  # Saltar este frame y continuar con el siguiente
 
-        gray = improve_image_quality(frame)
-        caras = detector(gray)
+            gray = improve_image_quality(frame)
+            caras = detector(gray)
 
-        # Verificar si se ha reiniciado la cámara
-        if not last_result:
-            descriptor = None
+            # Verificar si se ha reiniciado la cámara
+            if not last_result:
+                descriptor = None
 
-        # Actualizar los descriptores faciales y nombres de la base de datos en cada iteración
-        if not processing:
-            names_descriptors_from_db = get_facial_descriptors_and_names_from_db()
+            # Actualizar los descriptores faciales y nombres de la base de datos en cada iteración
+            if not processing:
+                names_descriptors_from_db = get_facial_descriptors_and_names_from_db()
 
-        # Verificar si es el primer rostro detectado y compararlo con los descriptores de la base de datos
-        if descriptor is None and not primer_rostro_detectado:
-            if len(caras) > 0:
-                if stable_face_count < stable_face_threshold:
-                    stable_face_count += 1
-                    detected_face = caras[0]
-                else:
-                    primer_rostro_detectado = True
-                    forma = predictor(gray, detected_face)
-                    descriptor = np.array(facial_recognition_model.compute_face_descriptor(frame, forma))
-                    stable_face_count = 0  # Resetear el contador de estabilidad
+            # Verificar si es el primer rostro detectado y compararlo con los descriptores de la base de datos
+            if descriptor is None and not primer_rostro_detectado:
+                if len(caras) > 0:
+                    if stable_face_count < stable_face_threshold:
+                        stable_face_count += 1
+                        detected_face = caras[0]
+                    else:
+                        primer_rostro_detectado = True
+                        forma = predictor(gray, detected_face)
+                        descriptor = np.array(facial_recognition_model.compute_face_descriptor(frame, forma))
+                        stable_face_count = 0  # Resetear el contador de estabilidad
 
-        # Si se ha detectado el primer rostro y hay caras detectadas, proceder con la comparación de descriptores faciales
-        current_time = cv2.getTickCount() / cv2.getTickFrequency()
-        if primer_rostro_detectado and len(caras) > 0 and (current_time - last_compare_time) >= compare_interval:
-            last_compare_time = current_time
-            for descriptor_actual in [descriptor]:
-                for nie, name, bachillerato, descriptor_db, imagen_blob in names_descriptors_from_db:
-                    distance_value = distance.euclidean(descriptor_actual, descriptor_db)
-                    umbral = 0.5
-                    if distance_value < umbral:
-                        last_result = f"MATCH: {name}"
-                        imagen_base64 = base64.b64encode(imagen_blob).decode('utf-8')
-                        student_info = f"{nie},{name},{bachillerato},{imagen_base64}"
+            # Si se ha detectado el primer rostro y hay caras detectadas, proceder con la comparación de descriptores faciales
+            current_time = cv2.getTickCount() / cv2.getTickFrequency()
+            if primer_rostro_detectado and len(caras) > 0 and (current_time - last_compare_time) >= compare_interval:
+                last_compare_time = current_time
+                for descriptor_actual in [descriptor]:
+                    for nie, name, bachillerato, descriptor_db, imagen_blob in names_descriptors_from_db:
+                        distance_value = distance.euclidean(descriptor_actual, descriptor_db)
+                        umbral = 0.5
+                        if distance_value < umbral:
+                            last_result = f"MATCH: {name}"
+                            imagen_base64 = base64.b64encode(imagen_blob).decode('utf-8')
+                            student_info = f"{nie},{name},{bachillerato},{imagen_base64}"
+                            break
+                    if last_result is not None:
                         break
-                if last_result is not None:
-                    break
 
-        # Si no se encontró ninguna coincidencia y se detectó el primer rostro, establecer last_result en un valor que indique que el estudiante no está registrado
-        if last_result is None and primer_rostro_detectado:
-            last_result = "Estudiante no registrado"
+            # Si no se encontró ninguna coincidencia y se detectó el primer rostro, establecer last_result en un valor que indique que el estudiante no está registrado
+            if last_result is None and primer_rostro_detectado:
+                last_result = "Estudiante no registrado"
 
-        # Función para reiniciar los valores después de 5 segundos
-        if not processing:
-            threading.Timer(3, reset_values).start()
-            processing = True  
+            # Función para reiniciar los valores después de 5 segundos
+            if not processing:
+                threading.Timer(5, reset_values).start()
+                processing = True
 
-        # Dibujar un rectángulo alrededor de las caras detectadas y mostrar el resultado en el frame
-        for cara in caras:
-            x, y, w, h = cara.left(), cara.top(), cara.width(), cara.height()
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            # Dibujar un rectángulo alrededor de las caras detectadas y mostrar el resultado en el frame
+            for cara in caras:
+                x, y, w, h = cara.left(), cara.top(), cara.width(), cara.height()
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-        # Añadir el texto al frame
-        if last_result is not None:
-            cv2.putText(frame, last_result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            # Añadir el texto al frame
+            if last_result is not None:
+                cv2.putText(frame, last_result, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-        # Codificar el frame como JPEG para la transmisión
-        (flag, encodedImage) = cv2.imencode(".jpg", frame)
-        if not flag:
-            continue
+            # Codificar el frame como JPEG para la transmisión
+            (flag, encodedImage) = cv2.imencode(".jpg", frame)
+            if not flag:
+                continue
 
-        # Enviar el frame codificado a través de SSE
-        yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
+            # Enviar el frame codificado a través de SSE
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + bytearray(encodedImage) + b"\r\n"
 
-        if student_info is not None:
-            yield b"data: " + student_info.encode() + b"\n\n"
+            if student_info is not None:
+                yield b"data: " + student_info.encode() + b"\n\n"
+        except Exception as e:
+            print(f"Error: {e}")
+            continue  # Manejar cualquier otro error y continuar con el siguiente frame
 
 # Asegúrate de que esta parte esté dentro de la función process_video
 if cap is not None:
